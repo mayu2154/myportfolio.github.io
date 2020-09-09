0726996eae2566ebf55946b1ef8d3f642dab8497
mayu2154 <69712167+mayu2154@users.noreply.github.com>
2020-09-09 15:15:04 +0530

    update

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -27,19 +27,5 @@
     {
         header('location:.index.html');
     }
-
-
-
-
-    $msg = "";
-    if(isset($_GET['error']))
-    {
-         $msg = "Please Fill in the blanks ";
-         echo "<div class="alert alert-danger">".$msg."</div>"; 
-    }
-    if(isset($_GET['success']))
-    {
-         $msg = "Your Message has been sent ";
-         echo "<div class="alert alert-success">".$msg."</div>"; 
-    }
+    
 ?>
