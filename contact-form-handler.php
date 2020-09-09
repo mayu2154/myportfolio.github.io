4d5e517472f65f8f2026a0fc28ea46103ae6d1b2
mayu2154 <69712167+mayu2154@users.noreply.github.com>
2020-09-09 15:58:01 +0530

    last time

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,46 +1,46 @@
 <?php
-   if(isset($_POST['submit']))
-   {
-      $name=$_POST ['name'];
-      $email=$_POST['email'];
-      $No=$_POST['no'];
+//    if(isset($_POST['submit']))
+//    {
+//       $name=$_POST ['name'];
+//       $email=$_POST['email'];
+//       $No=$_POST['no'];
 
       $message="Thank you!";
 
       
       $subject="Form submission";
 
-      if(empty($name) || empty($email) || empty($NO) || empty($msg))
-      {
-          header('location:.index.html?error');
-      }
-      else
-      {
+//       if(empty($name) || empty($email) || empty($NO) || empty($msg))
+//       {
+          $headers='location:.index.html?error';
+//       }
+//       else
+//       {
         $to='mayurinarkhede2154@gmail.com';
 
         if(mail ($to, $subject, $message, $headers))
         {
             header("location:index.html?success");
         }        
-       }
-   }
-    else
-    {
-        header('location:.index.html');
-    }
+//        }
+//    }
+//     else
+//     {
+//         header('location:.index.html');
+//     }
     
 
-    $message = "";
-    if(isset($_GET['error']))
-    {
-         $message = "Please Fill in the blanks ";
-         echo "<div class="alert alert-danger">".$message."</div>"; 
-    }
-    if(isset($_GET['success']))
-    {
-         $message = "Your Message has been sent ";
-         echo "<div class="alert alert-success">".$message."</div>"; 
-    }
+    // $message = "";
+    // if(isset($_GET['error']))
+    // {
+    //      $message = "Please Fill in the blanks ";
+    //      echo "<div class="alert alert-danger">".$message."</div>"; 
+    // }
+    // if(isset($_GET['success']))
+    // {
+    //      $message = "Your Message has been sent ";
+    //      echo "<div class="alert alert-success">".$message."</div>"; 
+    // }
 
 
 // $to="mayurinarkhede2154@gmail.com";
