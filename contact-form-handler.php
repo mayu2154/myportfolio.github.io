fd119eeefbc4598ea1716e4bfc0978bac02899f0
mayu2154 <69712167+mayu2154@users.noreply.github.com>
2020-09-09 16:11:54 +0530

    oyeee

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -70,17 +70,17 @@
 $name=$_POST ['name'];
 $visitor_email=$_POST['email'];
 $Mobno=$_POST['no'];
-$email_from='mayurinarkhede2154@gmail.com';
+$email_from='yugalmali198@gmail.com';
 
-$email_subjec="New form submission";
+$email_subject="New form submission";
 $email_body="User name :$name.\n".
             "User Email:$visitor_email.\n".
             "user Mobno:$Mobno.\n";
     $to="mayurinarkhede2154@gmail.com";
     $headers="From:$email_from\r\n";
     $headers="Reply-To:$visitor_email\r\n";
-     mail($to,$email_subjec,$email_body,$headers);
-     header("location:index.html");
+     mail($to,$email_subject,$email_body,$headers);
+     header("location: index.html");
             
 
 ?>
